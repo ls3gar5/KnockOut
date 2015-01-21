1be41a5e44f5a148540db06e8d749b2475944260
Leo <lsegars@gmail.com>
2015-01-21 11:27:52 -0300

    Arranque Inicial con todo

---
+++ KnockOut/ViewModel/fullIntegration.js
@@ -0,0 +1,21 @@
+﻿$(document).ready(function () {
+
+    function PersonViewModel() {
+        var self = this;
+
+        self.firstName = ko.observable("Leo");
+        self.lastName = ko.observable("Segars");
+
+        self.fullName = ko.computed(function () {
+            return self.firstName() + " - " + self.lastName() ;
+        });
+
+        self.checkOut = function () {
+            toastr.info("Hola");
+        };
+
+    };
+
+    ko.applyBindings(new PersonViewModel());
+
+});
